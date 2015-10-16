f012b961f64467d3747d8d94fd64283e73080944
Diego Gil <diego.gil@maipucinos.com.ar>
2015-10-16 18:07:29 -0300

    Missing fix

--- src/main/java/org/broadinstitute/dsde/consent/ontology/datause/ontologies/OntologyList.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/datause/ontologies/OntologyList.java
@@ -50,6 +50,7 @@ public class OntologyList implements OntologyModel {
                 OntologyLoader.loadOntology(is, umodel);
             }
         }
+        ((PelletInfGraph) umodel.getGraph()).classify();
         return umodel;
     }
 
