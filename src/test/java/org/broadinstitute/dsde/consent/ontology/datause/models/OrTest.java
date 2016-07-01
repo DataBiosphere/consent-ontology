027f7afd33d96e538b5188970897596a974f9329
root <leoforconesi@gmail.com>
2016-07-01 15:38:35 -0300

    Improve ontology testing

---
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/datause/models/OrTest.java
@@ -0,0 +1,10 @@
+package org.broadinstitute.dsde.consent.ontology.datause.models;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by root on 05/07/16.
+ */
+public class OrTest {
+
+}
---
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/resources/ErrorResponseTest.java
@@ -0,0 +1,29 @@
+package org.broadinstitute.dsde.consent.ontology.resources;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class ErrorResponseTest {
+
+    @Mock
+
+    ErrorResponse errorResponse;
+    private String messageMock;
+    private Integer codemock;
+
+//    @Before
+//            MockitoAnnotations.initMocks(this)
+
+    @Test
+    public void setMessage() throws Exception {
+
+    }
+
+}
