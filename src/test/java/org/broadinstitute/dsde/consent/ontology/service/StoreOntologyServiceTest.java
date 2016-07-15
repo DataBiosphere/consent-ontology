e9a682243347a34c9b3e26708dd6b3f77e55ccc3
Greg Rushton <grushton@broadinstitute.org>
2016-07-15 10:07:35 -0400

    Fix test

--- src/test/java/org/broadinstitute/dsde/consent/ontology/service/StoreOntologyServiceTest.java
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/service/StoreOntologyServiceTest.java
@@ -60,8 +60,9 @@ public class StoreOntologyServiceTest {
     public void testRetrieveOntologyURLs() throws Exception {
         HttpResponse httpResponse = getHttpResponse(urls);
         when(store.getStorageDocument(Mockito.anyString())).thenReturn(httpResponse);
-        List<URL> urls = (ArrayList)storeOntologyService.retrieveOntologyURLs();
-        urls.get(0).equals(new URL("http://www.broadinstitute.org/ontologies/DURPO/research_type"));
+        List<URL> urls = new ArrayList<>(storeOntologyService.retrieveOntologyURLs());
+        assertThat(!urls.isEmpty());
+        assertThat(urls.get(0).equals(new URL("http://www.broadinstitute.org/ontologies/DUOS/research_type")));
     }
 
     @Test
