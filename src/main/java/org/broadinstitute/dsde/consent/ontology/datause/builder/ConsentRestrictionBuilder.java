bd7bd3edd8aa62fcd51e0d37d3f99734af5b38d0
Greg Rushton <grushton@broadinstitute.org>
2018-04-05 07:42:04 -0400

    add gru sub-condition checks for methods and controls

--- src/main/java/org/broadinstitute/dsde/consent/ontology/datause/builder/ConsentRestrictionBuilder.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/datause/builder/ConsentRestrictionBuilder.java
@@ -37,7 +37,7 @@ import static org.broadinstitute.dsde.consent.ontology.datause.builder.UseRestri
  *  - Ethics Approval Required
  *  - Geographical Restrictions
  *
- *  Future expansions to DUOS could make use of these fields
+ *  Future additions to DUOS matching logic should make use of these fields.
  *
  */
 public class ConsentRestrictionBuilder implements UseRestrictionBuilder {
@@ -46,22 +46,26 @@ public class ConsentRestrictionBuilder implements UseRestrictionBuilder {
         List<UseRestriction> categoryRestrictions = new ArrayList<>();
         UseRestriction restriction;
 
+        // Self explanatory
         if (!dataUse.getDiseaseRestrictions().isEmpty()) {
             categoryRestrictions.add(
                 buildORRestrictionFromClasses(dataUse.getDiseaseRestrictions())
             );
         }
 
+        // Self explanatory
         if (!dataUse.getPopulationRestrictions().isEmpty()) {
             categoryRestrictions.add(
                 buildORRestrictionFromClasses(dataUse.getPopulationRestrictions())
             );
         }
 
+        // TRUE: Future commercial use is prohibited
         if (getOrElseFalse(dataUse.getCommercialUse())) {
             categoryRestrictions.add(new Not(new Named(NON_PROFIT)));
         }
 
+        // Gender/Pediatric checks.
         if (isPresent(dataUse.getGender()) &&
             getOrElseFalse(dataUse.getPediatric())) {
             if (dataUse.getGender().equalsIgnoreCase("male")) {
@@ -81,11 +85,12 @@ public class ConsentRestrictionBuilder implements UseRestrictionBuilder {
             categoryRestrictions.add(new Named(PEDIATRIC));
         }
 
-        // See GAWB-3210: In the case where GRU is sent in combination with other sub-conditions,
+        // GAWB-3210: In the case where GRU is sent in combination with other sub-conditions,
         // ignore GRU and apply those other restrictions instead.
-        // TODO: Work with Moran to see if we need to check for METHODS_RESEARCH/CONTROL here.
         if ((isPresent(dataUse.getGeneralUse()) && dataUse.getGeneralUse())
-                && categoryRestrictions.isEmpty()) {
+                && categoryRestrictions.isEmpty()
+                && !isMethodsResearchProhibited(dataUse)
+                && !isControlSetUsageProhibited(dataUse)) {
             return new Everything();
         }
 
@@ -99,7 +104,8 @@ public class ConsentRestrictionBuilder implements UseRestrictionBuilder {
         }
 
         // Apply Methods Research Logic
-        if (getOrElseTrue(dataUse.getMethodsResearch())) {
+        // TRUE: Future use for methods research (analytic/software/technology development) is prohibited
+        if (isMethodsResearchProhibited(dataUse)) {
             restriction = new Or(new Named(METHODS_RESEARCH), restriction);
         } else {
             restriction = new Or(
@@ -109,7 +115,8 @@ public class ConsentRestrictionBuilder implements UseRestrictionBuilder {
         }
 
         // Apply Control Set Logic
-        if (isPresent(dataUse.getControlSetOption()) && dataUse.getControlSetOption().equalsIgnoreCase("Yes")) {
+        // YES: Future as a control set for diseases other than those specified is prohibited
+        if (isControlSetUsageProhibited(dataUse)) {
             restriction = new Or(
                 restriction,
                 new And(restriction, new Named(CONTROL))
@@ -117,6 +124,17 @@ public class ConsentRestrictionBuilder implements UseRestrictionBuilder {
         }
 
         return restriction;
+    }
+
+    private Boolean isMethodsResearchProhibited(DataUse dataUse) {
+        if (isPresent(dataUse.getMethodsResearch()))
+            return dataUse.getMethodsResearch();
+        return false;
+    }
+
+    private Boolean isControlSetUsageProhibited(DataUse dataUse) {
+        return isPresent(dataUse.getControlSetOption()) &&
+                dataUse.getControlSetOption().equalsIgnoreCase("Yes");
     }
 
 }
