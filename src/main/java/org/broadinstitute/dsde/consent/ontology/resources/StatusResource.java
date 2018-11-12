1b6593549e16b18e45ccf3817655578dbdda670f
Greg Rushton <grushton@broadinstitute.org>
2018-11-12 13:02:10 -0500

    add status logging

--- src/main/java/org/broadinstitute/dsde/consent/ontology/resources/StatusResource.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/resources/StatusResource.java
@@ -2,6 +2,7 @@ package org.broadinstitute.dsde.consent.ontology.resources;
 
 import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.health.HealthCheckRegistry;
+import org.apache.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,6 +12,10 @@ import java.util.Map;
 
 @Path("status")
 public class StatusResource {
+
+    private Logger logger() {
+        return Logger.getLogger(StatusResource.class);
+    }
 
     private HealthCheckRegistry healthChecks;
 
@@ -22,7 +27,21 @@ public class StatusResource {
     @Produces("application/json")
     public Response getStatus() {
         Map<String, HealthCheck.Result> results = healthChecks.runHealthChecks();
+        // Ontology can work in a degraded status so log errors at the warning level.
+        results.entrySet().
+                stream().
+                filter(e -> !e.getValue().isHealthy()).
+                forEach(e -> logger().warn("Error in service " + e.getKey() + ": " + formatResultError(e.getValue())));
         return Response.ok(results).build();
+    }
+
+    private String formatResultError(HealthCheck.Result result) {
+        if (result.getMessage() != null) {
+            return result.getMessage();
+        } else if (result.getError() != null) {
+            return result.getError().toString();
+        }
+        return "Healthcheck Result Error";
     }
 
 }
