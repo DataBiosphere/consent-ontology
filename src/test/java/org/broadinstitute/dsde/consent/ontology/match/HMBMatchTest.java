9b9990fe50f1b4ae5b63c5f8f71bcd8260840da6
Greg Rushton <grushton@broadinstitute.org>
2018-12-15 17:56:36 -0500

    revert some test changes

--- src/test/java/org/broadinstitute/dsde/consent/ontology/match/HMBMatchTest.java
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/match/HMBMatchTest.java
@@ -19,25 +19,25 @@ public class HMBMatchTest extends MatchTestBase {
     public static Collection<Object[]> tests() {
         // Note that the purposes in this truth table are exactly the same as MRP, so we reuse them below
         return Arrays.asList(new Object[][]{
-            { Fixtures.MRP.mrpa, Fixtures.HMB.uc1, true, "MRPA vs. UC1 [true]" },
-            { Fixtures.MRP.mrpa, Fixtures.HMB.uc2, true, "MRPA vs. UC2 [true]" },
-            { Fixtures.MRP.mrpa, Fixtures.HMB.uc3, true, "MRPA vs. UC3 [true]" },
-            { Fixtures.MRP.mrpa, Fixtures.HMB.uc4, true, "MRPA vs. UC4 [true]" },
+            { Fixtures.MRP.mrpa, Fixtures.HMB.uc1, true, "MRPA vs. UC1" },
+            { Fixtures.MRP.mrpa, Fixtures.HMB.uc2, true, "MRPA vs. UC2" },
+            { Fixtures.MRP.mrpa, Fixtures.HMB.uc3, true, "MRPA vs. UC3" },
+            { Fixtures.MRP.mrpa, Fixtures.HMB.uc4, true, "MRPA vs. UC4" },
 
-            { Fixtures.MRP.mrpb, Fixtures.HMB.uc1, true, "MRPB vs. UC1 [true]" },
-            { Fixtures.MRP.mrpb, Fixtures.HMB.uc2, true, "MRPB vs. UC2 [true]" },
-            { Fixtures.MRP.mrpb, Fixtures.HMB.uc3, true, "MRPB vs. UC3 [true]" },
-            { Fixtures.MRP.mrpb, Fixtures.HMB.uc4, true, "MRPB vs. UC4 [true]" },
+            { Fixtures.MRP.mrpb, Fixtures.HMB.uc1, true, "MRPB vs. UC1" },
+            { Fixtures.MRP.mrpb, Fixtures.HMB.uc2, true, "MRPB vs. UC2" },
+            { Fixtures.MRP.mrpb, Fixtures.HMB.uc3, true, "MRPB vs. UC3" },
+            { Fixtures.MRP.mrpb, Fixtures.HMB.uc4, true, "MRPB vs. UC4" },
 
-            { Fixtures.MRP.mrpc, Fixtures.HMB.uc1, true, "MRPC vs. UC1 [true]" },
-//            { Fixtures.MRP.mrpc, Fixtures.HMB.uc2, false, "MRPC vs. UC2 [false]" }, // TODO: fix test. Doc says match should fail. This is also in conflict with new rules
-//            { Fixtures.MRP.mrpc, Fixtures.HMB.uc3, false, "MRPC vs. UC3 [false]" }, // TODO: fix test. Doc says match should fail. This is also in conflict with new rules
-            { Fixtures.MRP.mrpc, Fixtures.HMB.uc4, true, "MRPC vs. UC4 [true]" },
+            { Fixtures.MRP.mrpc, Fixtures.HMB.uc1, true, "MRPC vs. UC1" },
+            //  { Fixtures.MRP.mrpc, Fixtures.HMB.uc2, false, "MRPC vs. UC2" }, // TODO: fix test. Doc says match should fail. This is also in conflict with new rules
+            //  { Fixtures.MRP.mrpc, Fixtures.HMB.uc3, false, "MRPC vs. UC3" }, // TODO: fix test. Doc says match should fail. This is also in conflict with new rules
+            { Fixtures.MRP.mrpc, Fixtures.HMB.uc4, true, "MRPC vs. UC4" },
 
-            { Fixtures.CS.csa, Fixtures.HMB.uc1, true, "CSA vs. UC1 [true]" },
-            { Fixtures.CS.csb, Fixtures.HMB.uc1, true, "CSB vs. UC1 [true]" },
-            { Fixtures.CS.csc, Fixtures.HMB.uc1, true, "CSC vs. UC1 [true]" },
-            { Fixtures.CS.csd, Fixtures.HMB.uc1, true, "CSD vs. UC1 [true]" }
+            { Fixtures.CS.csa, Fixtures.HMB.uc1, true, "CSA vs. UC1" },
+            { Fixtures.CS.csb, Fixtures.HMB.uc1, true, "CSB vs. UC1" },
+            { Fixtures.CS.csc, Fixtures.HMB.uc1, true, "CSC vs. UC1" },
+            { Fixtures.CS.csd, Fixtures.HMB.uc1, true, "CSD vs. UC1" }
         });
     }
     
