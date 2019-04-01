e35e83a6dbc8b396d9f674635facc92cebccc2d9
Greg Rushton <rushtong@users.noreply.github.com>
2019-04-01 08:44:59 -0400

    [DUOS=233] Upgrade lucene (#134)
    * upgrade lucene, clean up unused lucene directory stuff

--- src/main/java/org/broadinstitute/dsde/consent/ontology/datause/api/LuceneOntologyTermSearchAPI.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/datause/api/LuceneOntologyTermSearchAPI.java
@@ -2,20 +2,23 @@ package org.broadinstitute.dsde.consent.ontology.datause.api;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.util.Version;
 import org.broadinstitute.dsde.consent.ontology.datause.models.OntologyTerm;
 import org.broadinstitute.dsde.consent.ontology.service.StoreOntologyService;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.HasClassesInSignature;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAnnotationValueVisitorEx;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.search.EntitySearcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +27,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -52,109 +59,94 @@ public class LuceneOntologyTermSearchAPI implements OntologyTermSearchAPI {
     }
 
     private void initAPI() throws OWLOntologyCreationException, IOException {
-
         Collection<String> resources = storeOntologyService.retrieveConfigurationKeys();
-        Directory indexDirectory = new RAMDirectory();
+        for (String resource : resources) {
+            try (InputStream stream = new URL(resource).openStream()) {
+                OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+                OWLOntology ontology = manager.loadOntologyFromOntologyDocument(stream);
+                HashMap<String, OWLAnnotationProperty> annotationProperties = new HashMap<>();
 
-        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_4_9);
+                ontology.annotationPropertiesInSignature().forEach((property) -> {
+                    if (property.getIRI().getRemainder().isPresent()) {
+                        annotationProperties.put(property.getIRI().getRemainder().get(), property);
+                    }
+                });
 
-        try (IndexWriter indexWriter = new IndexWriter(indexDirectory,
-                new IndexWriterConfig(Version.LUCENE_4_9, analyzer))) {
-            for (String resource : resources) {
-                try (InputStream stream = new URL(resource).openStream()) {
-                    OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-                    OWLOntology ontology = manager.loadOntologyFromOntologyDocument(stream);
-                    HashMap<String, OWLAnnotationProperty> annotationProperties = new HashMap<>();
+                OWLAnnotationProperty hasExactSynonym = annotationProperties.get("hasExactSynonym");
+                OWLAnnotationProperty label = annotationProperties.get("label");
+                OWLAnnotationProperty comment = annotationProperties.get("comment");
+                OWLAnnotationProperty def = annotationProperties.get(FIELD_DEFINITION_CLASS);
+                OWLAnnotationProperty deprecated = annotationProperties.get("deprecated");
 
-                    ontology.annotationPropertiesInSignature().forEach((property) -> {
-                        if (property.getIRI().getRemainder().isPresent()) {
-                            annotationProperties.put(property.getIRI().getRemainder().get(), property);
+                Set<OWLClass> owlClasses = ontology.classesInSignature().collect(Collectors.toSet());
+                owlClasses.addAll(ontology.
+                        directImports().
+                        flatMap(HasClassesInSignature::classesInSignature).
+                        collect(Collectors.toSet()));
+
+                owlClasses.forEach((OWLClass owlClass) -> {
+                    OWLAnnotationValueVisitorEx<String> visitor = new OWLAnnotationValueVisitorEx<String>() {
+                        @Override
+                        public String visit(IRI iri) {
+                            return iri.toString();
                         }
-                    });
 
-                    OWLAnnotationProperty hasExactSynonym = annotationProperties.get("hasExactSynonym");
-                    OWLAnnotationProperty label = annotationProperties.get("label");
-                    OWLAnnotationProperty comment = annotationProperties.get("comment");
-                    OWLAnnotationProperty def = annotationProperties.get(FIELD_DEFINITION_CLASS);
-                    OWLAnnotationProperty deprecated = annotationProperties.get("deprecated");
+                        @Override
+                        public String visit(OWLAnonymousIndividual owlAnonymousIndividual) {
+                            return owlAnonymousIndividual.toStringID();
+                        }
 
-                    Set<OWLClass> owlClasses = ontology.classesInSignature().collect(Collectors.toSet());
-                    owlClasses.addAll(ontology.
-                            directImports().
-                            flatMap(HasClassesInSignature::classesInSignature).
-                            collect(Collectors.toSet()));
+                        @Override
+                        public String visit(OWLLiteral owlLiteral) {
+                            return owlLiteral.getLiteral();
+                        }
+                    };
 
-                    owlClasses.forEach((OWLClass owlClass) -> {
-                        OWLAnnotationValueVisitorEx<String> visitor = new OWLAnnotationValueVisitorEx<String>() {
-                            @Override
-                            public String visit(IRI iri) {
-                                return iri.toString();
-                            }
-
-                            @Override
-                            public String visit(OWLAnonymousIndividual owlAnonymousIndividual) {
-                                return owlAnonymousIndividual.toStringID();
-                            }
-
-                            @Override
-                            public String visit(OWLLiteral owlLiteral) {
-                                return owlLiteral.getLiteral();
-                            }
-                        };
-
-                        // Do not index deprecated classes.
-                        if (!(deprecated != null && EntitySearcher.getAnnotations(owlClass, ontology, deprecated).count() > 0)) {
-                            Document document = new Document();
-                            if (hasExactSynonym != null) {
-                                EntitySearcher.getAnnotations(owlClass, ontology, hasExactSynonym).forEach((synonyms) ->
-                                        document.add(new TextField(FIELD_SYNONYM, synonyms.getValue().accept(visitor), Field.Store.YES)));
-                            }
-                            if (label != null) {
-                                Stream<OWLAnnotation> labels = EntitySearcher.getAnnotations(owlClass, ontology, label);
-                                ArrayList<OWLAnnotation> labelsList = labels.collect(Collectors.toCollection(ArrayList::new));
-                                assert labelsList.size() <= 1 : "Exactly 0 or 1 labels allowed per class";
-                                if (labelsList.size() == 1) {
-                                    document.add(new TextField(FIELD_LABEL, labelsList.stream().iterator().next().getValue().accept(visitor), Field.Store.YES));
-                                }
-                            }
-                            if (comment != null) {
-                                Stream<OWLAnnotation> comments = EntitySearcher.getAnnotations(owlClass, ontology, comment);
-                                ArrayList<OWLAnnotation> commentsList = comments.collect(Collectors.toCollection(ArrayList::new));
-
-                                assert commentsList.size() <= 1 : "Exactly 0 or 1 comments allowed per class";
-                                if (commentsList.size() == 1) {
-                                    document.add(new TextField(FIELD_COMMENT, commentsList.stream().iterator().next().getValue().accept(visitor), Field.Store.YES));
-                                }
-                            }
-                            if (def != null) {
-                                Stream<OWLAnnotation> defs = EntitySearcher.getAnnotations(owlClass, ontology, def);
-                                ArrayList<OWLAnnotation> defsList = defs.collect(Collectors.toCollection(ArrayList::new));
-                                assert defsList.size() <= 1 : "Exactly 0 or 1 definitions allowed per class";
-                                if (defsList.size() == 1) {
-                                    document.add(new TextField(FIELD_DEFINITION, defsList.stream().iterator().next().getValue().accept(visitor), Field.Store.YES));
-                                }
-                            }
-                            document.add(new TextField(FIELD_ID, owlClass.toStringID(), Field.Store.YES));
-                            nameToTerm.put(document.get(FIELD_ID),
-                                    new OntologyTerm(
-                                            document.get(FIELD_ID),
-                                            document.get(FIELD_COMMENT),
-                                            document.get(FIELD_LABEL),
-                                            document.get(FIELD_DEFINITION),
-                                            document.getValues(FIELD_SYNONYM)));
-
-
-                            try {
-                                indexWriter.addDocument(document);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e.getMessage());
+                    // Do not index deprecated classes.
+                    if (!(deprecated != null && EntitySearcher.getAnnotations(owlClass, ontology, deprecated).count() > 0)) {
+                        Document document = new Document();
+                        if (hasExactSynonym != null) {
+                            EntitySearcher.getAnnotations(owlClass, ontology, hasExactSynonym).forEach((synonyms) ->
+                                    document.add(new TextField(FIELD_SYNONYM, synonyms.getValue().accept(visitor), Field.Store.YES)));
+                        }
+                        if (label != null) {
+                            Stream<OWLAnnotation> labels = EntitySearcher.getAnnotations(owlClass, ontology, label);
+                            ArrayList<OWLAnnotation> labelsList = labels.collect(Collectors.toCollection(ArrayList::new));
+                            assert labelsList.size() <= 1 : "Exactly 0 or 1 labels allowed per class";
+                            if (labelsList.size() == 1) {
+                                document.add(new TextField(FIELD_LABEL, labelsList.stream().iterator().next().getValue().accept(visitor), Field.Store.YES));
                             }
                         }
-                    });
-                } catch (MalformedURLException e) {
-                    log.error("Unable to parse string to url: " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
+                        if (comment != null) {
+                            Stream<OWLAnnotation> comments = EntitySearcher.getAnnotations(owlClass, ontology, comment);
+                            ArrayList<OWLAnnotation> commentsList = comments.collect(Collectors.toCollection(ArrayList::new));
+
+                            assert commentsList.size() <= 1 : "Exactly 0 or 1 comments allowed per class";
+                            if (commentsList.size() == 1) {
+                                document.add(new TextField(FIELD_COMMENT, commentsList.stream().iterator().next().getValue().accept(visitor), Field.Store.YES));
+                            }
+                        }
+                        if (def != null) {
+                            Stream<OWLAnnotation> defs = EntitySearcher.getAnnotations(owlClass, ontology, def);
+                            ArrayList<OWLAnnotation> defsList = defs.collect(Collectors.toCollection(ArrayList::new));
+                            assert defsList.size() <= 1 : "Exactly 0 or 1 definitions allowed per class";
+                            if (defsList.size() == 1) {
+                                document.add(new TextField(FIELD_DEFINITION, defsList.stream().iterator().next().getValue().accept(visitor), Field.Store.YES));
+                            }
+                        }
+                        document.add(new TextField(FIELD_ID, owlClass.toStringID(), Field.Store.YES));
+                        nameToTerm.put(document.get(FIELD_ID),
+                                new OntologyTerm(
+                                        document.get(FIELD_ID),
+                                        document.get(FIELD_COMMENT),
+                                        document.get(FIELD_LABEL),
+                                        document.get(FIELD_DEFINITION),
+                                        document.getValues(FIELD_SYNONYM)));
+                    }
+                });
+            } catch (MalformedURLException e) {
+                log.error("Unable to parse string to url: " + e.getMessage());
+                throw new RuntimeException(e);
             }
         }
     }
