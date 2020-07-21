e239b8a881938fff2841629d3ec32330f8b8c4f7
Greg Rushton <rushtong@users.noreply.github.com>
2020-07-21 10:07:53 -0400

    [DUOS-709][risk=no] Revert plugin update, handle potential NPEs (#286)
    * revert plugin update, handle potential NPEs

--- src/main/java/org/broadinstitute/dsde/consent/ontology/resources/VersionResource.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/resources/VersionResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.nio.charset.Charset;
+import java.util.Objects;
 import java.util.Optional;
 
 @Path("/version")
@@ -47,8 +48,10 @@ public class VersionResource {
                 JsonObject jsonObject = new Gson().fromJson(props, JsonObject.class);
                 JsonElement shortHash = jsonObject.get("git.commit.id.abbrev");
                 JsonElement buildVersion = jsonObject.get("git.build.version");
-                this.hash = Optional.ofNullable(shortHash.getAsString()).orElse("error");
-                this.version = Optional.ofNullable(buildVersion.getAsString()).orElse("error");
+                if (Objects.nonNull(shortHash) && Objects.nonNull(buildVersion)) {
+                    this.hash = Optional.ofNullable(shortHash.getAsString()).orElse("error");
+                    this.version = Optional.ofNullable(buildVersion.getAsString()).orElse("error");
+                }
             }
         }
 
