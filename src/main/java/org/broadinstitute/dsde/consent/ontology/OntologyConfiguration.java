0154a74ee10ea8bea93fd86db4ec008efb20d545
Greg Rushton <grushton@broadinstitute.org>
2020-07-27 14:16:03 -0400

    remove unused cors configuration

--- src/main/java/org/broadinstitute/dsde/consent/ontology/OntologyConfiguration.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/OntologyConfiguration.java
@@ -3,39 +3,35 @@ package org.broadinstitute.dsde.consent.ontology;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import org.broadinstitute.dsde.consent.ontology.configurations.*;
+import org.broadinstitute.dsde.consent.ontology.configurations.ElasticSearchConfiguration;
+import org.broadinstitute.dsde.consent.ontology.configurations.StoreConfiguration;
+import org.broadinstitute.dsde.consent.ontology.configurations.StoreOntologyConfiguration;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OntologyConfiguration extends Configuration {
 
+  public OntologyConfiguration() {
+  }
 
-    public OntologyConfiguration() {}
+  @JsonProperty
+  private final ElasticSearchConfiguration elasticSearch = new ElasticSearchConfiguration();
 
-    @JsonProperty
-    private final ElasticSearchConfiguration elasticSearch = new ElasticSearchConfiguration();
+  @JsonProperty
+  private final StoreConfiguration googleStore = new StoreConfiguration();
 
-    @JsonProperty
-    private final StoreConfiguration googleStore = new StoreConfiguration();
+  @JsonProperty
+  private final StoreOntologyConfiguration storeOntology = new StoreOntologyConfiguration();
 
-    @JsonProperty
-    private final StoreOntologyConfiguration storeOntology = new StoreOntologyConfiguration();
+  public ElasticSearchConfiguration getElasticSearchConfiguration() {
+    return elasticSearch;
+  }
 
-    @JsonProperty
-    private final CorsConfiguration cors = new CorsConfiguration();
+  public StoreConfiguration getCloudStoreConfiguration() {
+    return googleStore;
+  }
 
-
-    public ElasticSearchConfiguration getElasticSearchConfiguration() {
-        return elasticSearch;
-    }
-
-    public StoreConfiguration getCloudStoreConfiguration() { return googleStore; }
-
-    public StoreOntologyConfiguration getStoreOntologyConfiguration() {
-        return storeOntology;
-    }
-
-    public CorsConfiguration getCorsConfiguration() {
-        return cors;
-    }
+  public StoreOntologyConfiguration getStoreOntologyConfiguration() {
+    return storeOntology;
+  }
 
 }
