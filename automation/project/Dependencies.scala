e99582f56cd4528462818f5e2853244532fb76e2
Greg Rushton <grushton@broadinstitute.org>
2020-08-27 17:57:05 -0400

    revert

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -5,7 +5,6 @@ object Dependencies {
   val gatlingV = "3.3.1"
 
   val rootDependencies: Seq[ModuleID] = Seq(
-    "com.typesafe"          % "config" % "1.4.0",
     "org.scalatest"        %% "scalatest" % "3.2.0" % Test,
     "io.gatling"            % "gatling-core" % gatlingV,
     "io.gatling"            % "gatling-http" % gatlingV,
