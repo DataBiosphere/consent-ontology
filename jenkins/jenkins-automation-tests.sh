35dd1d659c6ee6a1214e61e68e6406143df1800e
rfricke-asymmetrik <rfricke@asymmetrik.com>
2021-03-01 17:06:57 -0500

    DUOS-219 put some script elements back

--- jenkins/jenkins-automation-tests.sh
+++ jenkins/jenkins-automation-tests.sh
@@ -6,22 +6,22 @@ set -e
 # It is here because it is only intended to be run from a jenkins job and not by developers.
 
 FC_INSTANCE=${1:-$FC_INSTANCE}
-ENV=${2:-$ENV}#
+ENV=${2:-$ENV}
 TEST_IMAGE=automation-ontology
 
-#mkdir target
+mkdir target
 
 # Render Configurations
-#docker run --rm \
- #   -e ENVIRONMENT=${ENV} \
- #   -e ROOT_DIR="${PWD}" \
- #   -e FC_INSTANCE=${FC_INSTANCE} \
- #   -e OUT_PATH=/output/src/test/resources \
- #   -e INPUT_PATH=/input \
- #   -v /etc/vault-token-dsde:/root/.vault-token \
- #   -v "${PWD}/configs":/input \
- #   -v "${PWD}":/output \
- #   broadinstitute/dsde-toolbox:dev render-templates.sh
+docker run --rm \
+    -e ENVIRONMENT=${ENV} \
+    -e ROOT_DIR="${PWD}" \
+    -e FC_INSTANCE=${FC_INSTANCE} \
+    -e OUT_PATH=/output/src/test/resources \
+    -e INPUT_PATH=/input \
+    -v /etc/vault-token-dsde:/root/.vault-token \
+    -v "${PWD}/configs":/input \
+    -v "${PWD}":/output \
+    broadinstitute/dsde-toolbox:dev render-templates.sh
 
 
 # Build Test Image
