49f96fde6402a3444c3762cd0ae2c2d992c0ebc5
Greg Rushton <rushtong@users.noreply.github.com>
2021-08-17 12:29:07 -0400

    [DUOS-1398][risk=no] Update version to 12 character hash (#484)
    * update to 12 char hash

--- src/main/java/org/broadinstitute/dsde/consent/ontology/model/Version.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/model/Version.java
@@ -3,6 +3,7 @@ package org.broadinstitute.dsde.consent.ontology.model;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -16,10 +17,14 @@ public class Version {
       this.version = "error";
     } else {
       JsonObject jsonObject = new Gson().fromJson(props, JsonObject.class);
-      JsonElement shortHash = jsonObject.get("git.commit.id.abbrev");
+      String longHash = Optional
+          .ofNullable(jsonObject.get("git.commit.id"))
+          .orElse(new JsonPrimitive("error"))
+          .getAsString();
+      String shortHash = longHash.substring(0, Math.min(longHash.length(), 12));
       JsonElement buildVersion = jsonObject.get("git.build.version");
-      if (Objects.nonNull(shortHash) && Objects.nonNull(buildVersion)) {
-        this.hash = Optional.ofNullable(shortHash.getAsString()).orElse("error");
+      if (Objects.nonNull(buildVersion)) {
+        this.hash = shortHash;
         this.version = Optional.ofNullable(buildVersion.getAsString()).orElse("error");
       }
     }
