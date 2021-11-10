ac96bdeb6585697602873cad9f3b47d09d7db662
Gregory Rushton <rushtong@users.noreply.github.com>
2021-11-10 12:27:52 -0500

    [DUOS-1421][risk=no] Update status format (#527)
    * update status format to match consent

--- src/main/java/org/broadinstitute/dsde/consent/ontology/resources/StatusResource.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/resources/StatusResource.java
@@ -3,21 +3,27 @@ package org.broadinstitute.dsde.consent.ontology.resources;
 import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.health.HealthCheckRegistry;
 import com.google.inject.Inject;
-import org.broadinstitute.dsde.consent.ontology.Utils;
-import org.slf4j.Logger;
-
+import java.util.LinkedHashMap;
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.Map;
+import org.broadinstitute.dsde.consent.ontology.Utils;
+import org.broadinstitute.dsde.consent.ontology.cloudstore.GCSHealthCheck;
+import org.broadinstitute.dsde.consent.ontology.service.ElasticSearchHealthCheck;
+import org.slf4j.Logger;
 
 @Path("status")
 public class StatusResource {
 
+    public static final String OK = "ok";
+    public static final String DEGRADED = "degraded";
+    public static final String SYSTEMS = "systems";
+
     private final Logger log = Utils.getLogger(this.getClass());
 
-    private HealthCheckRegistry healthChecks;
+    private final HealthCheckRegistry healthChecks;
 
     @Inject
     public StatusResource(HealthCheckRegistry healthChecks) {
@@ -28,14 +34,28 @@ public class StatusResource {
     @Produces("application/json")
     public Response getStatus() {
         Map<String, HealthCheck.Result> results = healthChecks.runHealthChecks();
-        // Ontology can work in a degraded status so log errors at the warning level.
+        // Log errors at the warning level for follow-up
         results.entrySet().
                 stream().
                 filter(e -> !e.getValue().isHealthy()).
                 forEach(e -> log.warn("Error in service " + e.getKey() + ": " + formatResultError(e.getValue())));
-        return Response.ok(results).build();
+        return Response.ok(formatResults(results)).build();
     }
 
+    private Map<String, Object> formatResults(Map<String, HealthCheck.Result> results) {
+        // Order is important. Put Ok and Degraded states at the beginning of the map and
+        // add all other entries after that.
+        Map<String, Object> formattedResults = new LinkedHashMap<>();
+        // Ontology can still work in a degraded status
+        formattedResults.put(OK, true);
+        HealthCheck.Result gcs = results.getOrDefault(GCSHealthCheck.NAME, HealthCheck.Result.unhealthy("Unable to access GCS"));
+        HealthCheck.Result elasticSearch = results.getOrDefault(ElasticSearchHealthCheck.NAME, HealthCheck.Result.unhealthy("Unable to access ElasticSearch"));
+        boolean degraded = (!gcs.isHealthy() || !elasticSearch.isHealthy());
+        formattedResults.put(DEGRADED, degraded);
+        formattedResults.put(SYSTEMS, results);
+        return formattedResults;
+    }
+    
     private String formatResultError(HealthCheck.Result result) {
         if (result.getMessage() != null) {
             return result.getMessage();
