04dbd0874b832f1e24898126717637fa48c4efb1
Gregory Rushton <rushtong@users.noreply.github.com>
2022-01-27 09:47:12 -0500

    [DUOS-1598][risk=no] Upgrade mockito (#567)
    * upgrade mockito

--- src/test/java/org/broadinstitute/dsde/consent/ontology/match/MatchTestBase.java
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/match/MatchTestBase.java
@@ -1,7 +1,7 @@
 package org.broadinstitute.dsde.consent.ontology.match;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
