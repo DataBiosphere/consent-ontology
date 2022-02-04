46c995cf7a48d0d1b5038c9dcdf134e348412860
shaemarks <81024249+shaemarks@users.noreply.github.com>
2022-02-04 08:52:37 -0500

    [DUOS-1593][risk=no]ontology-endpoint-for-finding-multiple-doid-terms (#572)
    * Allow getOntologyById to handle a variable number of queries, separated my commas
    
    * Allow ontology queries with DOID in non-url form
    
    * Make field in OntologySearchResource final
    
    * Convert for each loop used to process id lookups to streams
    
    * Add tests for getting multiple ontologies by id
    
    Co-authored-by: Shae Marks <smarks.dev1@gmail.com>

--- src/main/java/org/broadinstitute/dsde/consent/ontology/resources/OntologySearchResource.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/resources/OntologySearchResource.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.dsde.consent.ontology.resources;
 
 import com.google.inject.Inject;
+import org.apache.commons.lang3.stream.Streams;
 import org.broadinstitute.dsde.consent.ontology.model.TermResource;
 import org.broadinstitute.dsde.consent.ontology.service.AutocompleteService;
 
@@ -10,13 +11,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Path("/search")
 public class OntologySearchResource {
 
-    private AutocompleteService service;
+    private final AutocompleteService service;
 
     @Inject
     public OntologySearchResource(AutocompleteService service) {
@@ -25,21 +28,39 @@ public class OntologySearchResource {
 
     @GET
     @Produces("application/json")
-    public Response getOntologyById(@QueryParam("id") @DefaultValue("") String queryTerm) throws IOException {
-        if (!queryTerm.isEmpty()) {
-            List<TermResource> result = service.lookupById(queryTerm);
-            if(!result.isEmpty()){
-                return Response.ok().entity(result).build();
-            } else {
-                return Response.status(Response.Status.NOT_FOUND)
-                        .entity(new ErrorResponse(" Supplied ID doesn't match any known ontology. ", Response.Status.NOT_FOUND.getStatusCode()))
-                        .build();
-            }
-        } else {
+    public Response getOntologyById(@QueryParam("id") @DefaultValue("") String queryTerm) {
+        if (queryTerm.isBlank()) {
             return Response
                     .status(Response.Status.BAD_REQUEST)
-                    .entity(new ErrorResponse(" Ontology ID term cannot be empty. ", Response.Status.BAD_REQUEST.getStatusCode()))
+                    .entity(new ErrorResponse("Ontology ID term cannot be empty.", Response.Status.BAD_REQUEST.getStatusCode()))
                     .build();
+        }
+
+        String[] queries = queryTerm.split(",");
+        try {
+            List<TermResource> results = new Streams.FailableStream<>(Arrays.stream(queries))
+                    .filter(Objects::nonNull)
+                    .filter(q -> !q.isBlank())
+                    .map(service::lookupById)
+                    .stream()
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
+            return checkOntologyRetrieval(results);
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new ErrorResponse("Ontology could not be successfully retrieved.", Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
+                    .build();
+        }
+
+    }
+
+    private Response checkOntologyRetrieval(List<TermResource> results) {
+        if (results.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErrorResponse("Supplied IDs do not match any known ontologies.", Response.Status.NOT_FOUND.getStatusCode()))
+                    .build();
+        } else {
+            return Response.ok().entity(results).build();
         }
     }
 }
