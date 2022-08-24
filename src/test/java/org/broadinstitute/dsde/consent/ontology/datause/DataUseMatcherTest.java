180aace72f58e7322efcc9b399a28641738cc755
Greg Rushton <grushton@broadinstitute.org>
2022-08-24 09:31:50 -0400

    fix import

--- src/test/java/org/broadinstitute/dsde/consent/ontology/datause/DataUseMatcherTest.java
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/datause/DataUseMatcherTest.java
@@ -1,7 +1,7 @@
 package org.broadinstitute.dsde.consent.ontology.datause;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mindswap.pellet.utils.ATermUtils.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
