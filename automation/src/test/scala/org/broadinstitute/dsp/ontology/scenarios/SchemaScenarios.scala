06aeeb588ab451358a04ed0cbfc68dc9f60aaa13
Greg Rushton <grushton@broadinstitute.org>
2023-03-07 13:04:35 -0500

    round 2: clean up pom and gatling tests

--- automation/src/test/scala/org/broadinstitute/dsp/ontology/scenarios/SchemaScenarios.scala
+++ automation/src/test/scala/org/broadinstitute/dsp/ontology/scenarios/SchemaScenarios.scala
@@ -12,41 +12,6 @@ class SchemaScenarios extends Simulation with TestRunner {
   runScenarios(
     List(
       GroupedScenario("Schema: DataUse") { exec(Requests.Schemas.dataUseSchema) },
-      GroupedScenario("Schema: Translate Consent General Use") {
-        exec(
-          Requests.Schemas.dataUseTranslateConsent(DataUseFactory.generalUse)
-            .check(jsonPath("$.type").find.is("everything"))
-        )
-      },
-      GroupedScenario("Schema: Translate Consent hmbResearch") {
-        exec(
-          Requests.Schemas.dataUseTranslateConsent(DataUseFactory.hmbResearch)
-            .check(jsonPath("$.type").find.is("or"))
-        )
-      },
-      GroupedScenario("Schema: Translate Consent hmbResearch Not General Use") {
-        exec(
-          Requests.Schemas.dataUseTranslateConsent(DataUseFactory.notGeneralHmb)
-        )
-      },
-      GroupedScenario("Schema: Translate Consent manualReview") {
-        exec(
-          Requests.Schemas.dataUseTranslateConsent(DataUseFactory.manualReview)
-        )
-      },
-      GroupedScenario("Schema: Translate DAR General Use") {
-        exec(
-          Requests.Schemas.dataUseTranslateDAR(DataUseFactory.generalUse)
-            .check(jsonPath("$.type").find.is("everything"))
-        )
-      },
-      GroupedScenario("Schema: Translate DAR manualReview") {
-        exec(
-          Requests.Schemas.dataUseTranslateDAR(DataUseFactory.manualReview)
-            .check(jsonPath("$.type").find.is("and"))
-            .check(jsonPath("$.operands[0].type").find.is("and"))
-        )
-      }
     )
   )
 
