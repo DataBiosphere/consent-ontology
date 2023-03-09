2e859ed2fb5a3075cfdaab5eae1ce2a28af17e30
koflaher <koflaher@broadinstitute.org>
2023-03-09 15:21:54 -0500

    initial changes to data use v3 validation

---
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/util/JsonSchemaUtil.java
@@ -0,0 +1,93 @@
+package org.broadinstitute.dsde.consent.ontology.util;
+
+import java.util.List;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.apache.commons.io.IOUtils;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.BadRequestException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
+
+public class JsonSchemaUtil {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final LoadingCache<String, String> cache;
+  private final String dataUseSchemaV3 = "/data-use-v3.json";
+
+  public JsonSchemaUtil() {
+    CacheLoader<String, String> loader = new CacheLoader<>() {
+      @Override
+      public String load(String key) throws Exception {
+        return IOUtils.resourceToString(key, Charset.defaultCharset());
+      }
+    };
+    this.cache = CacheBuilder.newBuilder().build(loader);
+  }
+
+  public String getDataUseSchemaV3() {
+    try {
+      return cache.get(dataUseSchemaV3);
+    } catch (ExecutionException ee) {
+      logger.error("Unable to load the data use schema V3: " + ee.getMessage());
+      return null;
+    }
+  }
+
+  /**
+   * Loads a Schema populated from the data use V3 schema
+   * @return Schema The Schema
+   * @throws ExecutionException Error reading from cache
+   */
+  private Schema getDataUseInstance() throws ExecutionException {
+    String schemaString = getDataUseSchemaV3();
+    JSONObject jsonSchema = new JSONObject(schemaString);
+    return SchemaLoader
+        .builder()
+        .schemaJson(jsonSchema)
+        .build()
+        .load()
+        .build();
+  }
+
+  /**
+   * Compares an instance of a data use object to the data use V3 schema
+   *
+   * @param dataUseInstance The string instance of a data use object
+   * @return True if the instance validates, false otherwise
+   */
+
+  public List<String> validateDataUseV3Schema(String dataUseInstance) {
+    try {
+      JSONObject jsonSubject = new JSONObject(dataUseInstance);
+      Schema schema = getDataUseInstance();
+      schema.validate(jsonSubject);
+      return List.of();
+    } catch (
+        ExecutionException ee) {
+      logger.error("Unable to load the data use schema: " + ee.getMessage());
+      return List.of(ee.getMessage());
+    } catch (ValidationException ve) {
+      if (logger.isDebugEnabled()) {
+        logger.debug("Provided instance does not validate: " + ve.getMessage());
+      }
+      if (logger.isTraceEnabled()) {
+        for (String m : ve.getAllMessages()) {
+          logger.trace("Validation error: " + m);
+        }
+      }
+      return ve.getAllMessages();
+    } catch (Exception e) {
+      throw new BadRequestException("Invalid schema");
+    }
+
+  }
+}
---
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/util/JsonSchemaUtilTest.java
@@ -0,0 +1,53 @@
+package org.broadinstitute.dsde.consent.ontology.util;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class JsonSchemaUtilTest {
+
+  private static JsonSchemaUtil schemaUtil;
+
+  private final String dataUseInstance = """
+      {
+      "generalUse": true,
+      "diseaseRestrictions":
+      "hmbResearch": true,
+      "populationOriginsAncestry": true,
+      "methodsResearch": true,
+      "commercialUse": true,
+      "nonProfitUse": true,
+      "other": "other",
+      "secondaryOther": "secondaryOther",
+      "ethicsApprovalRequired": true,
+      "collaboratorRequired": true,
+      "geographicalRestrictions": "geographicalRestrictions",
+      "geneticStudiesOnly": true,
+      "publicationResults": true,
+      "publicationMoratorium": "2023-03-09"
+      }
+      """;
+
+  @BeforeClass
+  public static void setUp() {
+    schemaUtil = new JsonSchemaUtil();
+  }
+
+  @Test
+  public void testIsValidDataUseV3Object_case0() {
+    String instance = "{}";
+    List<String> errors = schemaUtil.validateDataUseV3Schema(instance);
+    assertFalse(errors.isEmpty());
+  }
+
+  @Test
+  public void testIsValidDataUseV3Object_case1() {
+    List<String> errors = schemaUtil.validateDataUseV3Schema(dataUseInstance);
+    assertTrue(errors.isEmpty());
+  }
+}
