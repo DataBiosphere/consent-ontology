3bce1daad48ccd115af31d371b7f085de3afd208
koflaher <koflaher@broadinstitute.org>
2023-03-21 17:02:45 -0400

    WIP Translate V3 Business Logic into Code

---
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/datause/DataUseMatchCasesV3.java
@@ -0,0 +1,206 @@
+package org.broadinstitute.dsde.consent.ontology.datause;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.broadinstitute.dsde.consent.ontology.model.DataUseV3;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class DataUseMatchCasesV3 {
+  private static final String HMB_F1 = "The GRU Research Purpose does not match the HMB data use limitations.";
+  private static final String HMB_F2 = "The HMB Research Purpose does not match the Disease-Specific data use limitations.";
+  private static final String DS_F1 = "The unspecified Disease Research Purpose does not match the Disease-Specific data use limitations.";
+  private static final String DS_F2 = "The Disease-Specific: %s Research Purpose is not a valid subclass of the Disease-Specific data use limitations.";
+  private static final String MDS_F1 = "The Methods development Research Purpose does not match the Disease-Specific data use limitations.";
+  private static final String POA_F1 = "The Populations, Origins, Ancestry Research Purpose does not match the HMB or Disease-Specific data use limitation.";
+  private static final String NCU_F1 = "The Commercial Use Research Purpose does not match the No Commercial Use data use limitation.";
+
+  /**
+   * RP: Disease Focused Research
+   * Future use is limited to research involving the following disease area(s) [DS]
+   * Datasets:
+   *      Any dataset with GRU=true
+   *      Any dataset with HMB=true
+   *      Any dataset tagged to this disease exactly
+   *      Any dataset tagged to a DOID ontology Parent of disease X
+   *
+   * @param purpose The data use object representing the Research Purpose
+   * @param dataset The data use object representing the Dataset
+   * @param purposeDiseaseIdMap is a map of each purpose term id to a list of that term's parent term ids
+   */
+  static ImmutablePair<Boolean, List<String>> matchDiseases(
+      DataUseV3 purpose, DataUseV3 dataset, Map<String, List<String>> purposeDiseaseIdMap) {
+    // short-circuit if no disease focused research
+    if (purpose.getDiseaseRestrictions().isEmpty() && dataset.getDiseaseRestrictions().isEmpty()) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+    // short-circuit if dataset is GRU
+    if (getNullableOrFalse(dataset.getGeneralUse())) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+    // short-circuit if dataset is HMB
+    if (getNullableOrFalse(dataset.getHmbResearch())) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+    // short-circuit if no diseases specified in the purpose, but the dataset specifies diseases
+    if (!dataset.getDiseaseRestrictions().isEmpty() && purpose.getDiseaseRestrictions().isEmpty()) {
+      return ImmutablePair.of(false, Collections.singletonList(DS_F1));
+    }
+
+    // We want all-purpose disease IDs to be a subclass of any dataset disease ID
+    List<String> failures = new ArrayList<>();
+    for (Map.Entry<String, List<String>> entry : purposeDiseaseIdMap.entrySet()) {
+      boolean match = entry.getValue()
+          .stream()
+          .anyMatch(dataset.getDiseaseRestrictions()::contains);
+      if (!match) {
+        failures.add(String.format(DS_F2,  entry.getKey()));
+      }
+    }
+
+    return ImmutablePair.of(failures.isEmpty(), failures);
+  }
+
+  /**
+   * RP: Methods development/Validation study
+   * Future use for methods research (analytic/software/technology development) outside the
+   *      bounds of the other specified restrictions
+   * Datasets:
+   *      Any dataset where GRU = true
+   *      Any dataset where HMB = true
+   *      Any dataset where POA = true
+   *      Any dataset where DS-X match
+   */
+  static ImmutablePair<Boolean, List<String>> matchMDS(DataUseV3 purpose, DataUseV3 dataset, boolean diseaseMatch) {
+    // short-circuit if dataset is GRU
+    if (getNullableOrFalse(dataset.getGeneralUse())) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    // short-circuit if dataset is HMB
+    if (getNullableOrFalse(dataset.getHmbResearch())) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    // short-circuit if dataset is POA
+    if (getNullableOrFalse(dataset.getPopulationOriginsAncestry())) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    // short-circuit if there is a disease match
+    if (diseaseMatch) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    } else {
+      return ImmutablePair.of(false, Collections.singletonList(MDS_F1));
+    }
+  }
+
+  /**
+   * RP: Study population origins or ancestry
+   * Future use is limited to research involving a specific population [POA]
+   * Datasets:
+   *      Any dataset tagged with GRU
+   *      Any dataset tagged with POA
+   */
+  static ImmutablePair<Boolean, List<String>> matchPOA(DataUseV3 purpose, DataUseV3 dataset) {
+    // short-circuit if dataset is GRU
+    if (getNullableOrFalse(dataset.getGeneralUse())) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    // short-circuit if no POA clause
+    if (purpose.getPopulationOriginsAncestry() == null) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    List<String> failures = new ArrayList<>();
+    if (!(purpose.getPopulationOriginsAncestry() &&
+        getNullableOrFalse(dataset.getGeneralUse()))) {
+      failures.add(POA_F1);
+    }
+
+    return ImmutablePair.of(failures.isEmpty(), failures);
+  }
+
+  /**
+   * RP: Commercial purpose/by a commercial entity
+   * Future commercial use is prohibited [NCU]
+   * Future use by for-profit entities is prohibited [NPU]
+   * Datasets:
+   *      Any dataset where NPU and NCU are both false
+   */
+  static ImmutablePair<Boolean, List<String>> matchCommercial(DataUseV3 purpose, DataUseV3 dataset) {
+    // short-circuit if no commercial clause
+    if (purpose.getCommercialUse() == null || dataset.getCommercialUse() == null) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    List<String> failures = new ArrayList<>();
+    boolean purposeCommercial = getNullableOrFalse(purpose.getCommercialUse());
+    boolean datasetCommercial = getNullableOrFalse(dataset.getCommercialUse());
+
+    if (purposeCommercial && !datasetCommercial) {
+      failures.add(NCU_F1);
+    }
+
+    return ImmutablePair.of(failures.isEmpty(), failures);
+  }
+
+  /**
+   * RP: HMB
+   * Datasets:
+   *      Any dataset tagged with GRU
+   *      Any dataset tagged with HMB
+   *
+   * @param purpose The data use object representing the Research Purpose
+   * @param dataset The data use object representing the Dataset
+   */
+  static ImmutablePair<Boolean, List<String>> matchHMB(DataUseV3 purpose, DataUseV3 dataset) {
+    // short-circuit hmb if not set
+    if (purpose.getHmbResearch() == null && dataset.getHmbResearch() == null) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    boolean purposeGRU = getNullableOrFalse(purpose.getGeneralUse());
+    boolean purposeHMB = getNullableOrFalse(purpose.getHmbResearch());
+    boolean datasetGRU = getNullableOrFalse(dataset.getGeneralUse());
+    boolean datasetHMB = getNullableOrFalse(dataset.getHmbResearch());
+
+    if (datasetHMB && purposeGRU) {
+      return ImmutablePair.of(false, Collections.singletonList(HMB_F1));
+    }
+
+    if (purposeHMB && datasetGRU) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+    if (purposeHMB && datasetHMB) {
+      return ImmutablePair.of(true, Collections.emptyList());
+    }
+
+    else {
+      return ImmutablePair.of(false, Collections.singletonList(HMB_F2));
+    }
+
+  }
+
+  // Helper Methods
+
+  /**
+   * @param bool nullable boolean value
+   * @return boolean The value, or false otherwise
+   */
+  private static boolean getNullableOrFalse(Boolean bool) {
+    return Optional.ofNullable(bool).orElse(false);
+  }
+
+  /**
+   * @param yesOrNo nullable string value
+   * @return boolean True if "yes", false otherwise
+   */
+  private static boolean getNullable(String yesOrNo) {
+    return Optional.ofNullable(yesOrNo).orElse("no").equalsIgnoreCase("yes");
+  }
+
+}
---
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/datause/DataUseMatcherV3.java
@@ -0,0 +1,91 @@
+package org.broadinstitute.dsde.consent.ontology.datause;
+
+import com.google.inject.Inject;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.broadinstitute.dsde.consent.ontology.model.DataUseV3;
+import org.broadinstitute.dsde.consent.ontology.service.AutocompleteService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.broadinstitute.dsde.consent.ontology.datause.DataUseMatchCasesV3.matchCommercial;
+import static org.broadinstitute.dsde.consent.ontology.datause.DataUseMatchCasesV3.matchDiseases;
+import static org.broadinstitute.dsde.consent.ontology.datause.DataUseMatchCasesV3.matchHMB;
+import static org.broadinstitute.dsde.consent.ontology.datause.DataUseMatchCasesV3.matchMDS;
+import static org.broadinstitute.dsde.consent.ontology.datause.DataUseMatchCasesV3.matchPOA;
+
+public class DataUseMatcherV3 {
+
+  private AutocompleteService autocompleteService;
+
+  public DataUseMatcherV3() {
+  }
+
+  @Inject
+  public void setAutocompleteService(AutocompleteService autocompleteService) {
+    this.autocompleteService = autocompleteService;
+  }
+
+  // Matching Algorithm
+  public ImmutablePair<Boolean, List<String>> matchPurposeAndDatasetV3(DataUseV3 purpose, DataUseV3 dataset) {
+    Map<String, List<String>> purposeDiseaseIdMap;
+    try {
+      purposeDiseaseIdMap = generatePurposeDiseaseIdMap(purpose.getDiseaseRestrictions());
+    } catch (Exception e) {
+      String purposeRestrictions = StringUtils.join(purpose.getDiseaseRestrictions(), ", ");
+      List<String> errors = Arrays.asList(e.getMessage(), "Error found in one of the purpose terms: " + purposeRestrictions);
+      return ImmutablePair.of(false, errors);
+    }
+
+    ImmutablePair<Boolean, List<String>> diseaseMatch = matchDiseases(purpose, dataset, purposeDiseaseIdMap);
+    final List<ImmutablePair<Boolean, List<String>>> matchReasons = new ArrayList<>();
+    matchReasons.add(matchDiseases(purpose, dataset, purposeDiseaseIdMap));
+    matchReasons.add(matchHMB(purpose, dataset));
+    matchReasons.add(matchMDS(purpose, dataset, diseaseMatch.getLeft()));
+    matchReasons.add(matchPOA(purpose, dataset));
+    matchReasons.add(matchCommercial(purpose, dataset));
+    final Boolean match = matchReasons.stream().
+        map(ImmutablePair::getLeft).
+        allMatch(BooleanUtils::isTrue);
+    final List<String> reasons = matchReasons.stream().
+        map(ImmutablePair::getRight).
+        flatMap(Collection::stream).
+        filter(StringUtils::isNotBlank).
+        collect(Collectors.toList());
+    return ImmutablePair.of(match, reasons);
+  }
+
+  // Helper methods
+
+  // Get a map of disease term to list of parent term ids (which also includes disease term id)
+  private Map<String, List<String>> generatePurposeDiseaseIdMap(List<String> diseaseRestrictions) throws IOException {
+    Map<String, List<String>> map = new HashMap<>();
+    for (String r : diseaseRestrictions) {
+      map.put(r, getParentTermIds(r));
+    }
+    return map;
+  }
+
+  // Get a list of term ids that represent a disease term + all parent ids
+  private List<String> getParentTermIds(String purposeDiseaseId) throws IOException {
+    List<String> purposeTermIdList = autocompleteService.lookupById(purposeDiseaseId)
+        .stream()
+        .filter(Objects::nonNull)
+        .filter(t -> t.getParents() != null && !t.getParents().isEmpty())
+        .flatMap(t -> t.parents.stream())
+        .map(p -> p.id)
+        .collect(Collectors.toList());
+    purposeTermIdList.add(purposeDiseaseId);
+    return purposeTermIdList;
+  }
+
+}
