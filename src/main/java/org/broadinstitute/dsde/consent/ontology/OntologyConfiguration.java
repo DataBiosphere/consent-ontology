293a0cb551469baf1dafef017818de6661d6eb4f
Gregory Rushton <rushtong@users.noreply.github.com>
2023-05-02 09:17:59 -0400

    [DUOS-2461][risk=no] Remove unused configuration (#816)
    * rm unused

--- src/main/java/org/broadinstitute/dsde/consent/ontology/OntologyConfiguration.java
+++ src/main/java/org/broadinstitute/dsde/consent/ontology/OntologyConfiguration.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import org.broadinstitute.dsde.consent.ontology.configurations.ElasticSearchConfiguration;
 import org.broadinstitute.dsde.consent.ontology.configurations.StoreConfiguration;
-import org.broadinstitute.dsde.consent.ontology.configurations.StoreOntologyConfiguration;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OntologyConfiguration extends Configuration {
@@ -16,19 +15,12 @@ public class OntologyConfiguration extends Configuration {
   @JsonProperty
   private final StoreConfiguration googleStore = new StoreConfiguration();
 
-  @JsonProperty
-  private final StoreOntologyConfiguration storeOntology = new StoreOntologyConfiguration();
-
   public ElasticSearchConfiguration getElasticSearchConfiguration() {
     return elasticSearch;
   }
 
   public StoreConfiguration getCloudStoreConfiguration() {
     return googleStore;
-  }
-
-  public StoreOntologyConfiguration getStoreOntologyConfiguration() {
-    return storeOntology;
   }
 
 }
