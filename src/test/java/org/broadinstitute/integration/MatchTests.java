0748a53fbec7099a736d3f3852b5db4ce680bb22
rushtong <grushton@broadinstitute.org>
2024-04-22 12:54:16 -0400

    tests: update smoke tests to match current changes

--- src/test/java/org/broadinstitute/integration/MatchTests.java
+++ src/test/java/org/broadinstitute/integration/MatchTests.java
@@ -3,6 +3,7 @@ package org.broadinstitute.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.api.client.http.HttpStatusCodes;
+import jakarta.ws.rs.core.Response.Status;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,12 +26,19 @@ class MatchTests implements IntegrationTestHelper {
   @Test
   void ontology_match_v2_page_OK() throws Exception {
     SimpleResponse response = getPostResponse("match/v2", matchPair);
-    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.code());
+    assertEquals(Status.GONE.getStatusCode(), response.code());
   }
 
   @DisplayName("Match V3: OK test")
   @Test
   void ontology_match_v3_page_OK() throws Exception {
+    SimpleResponse response = getPostResponse("match/v3", matchPair);
+    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.code());
+  }
+
+  @DisplayName("Match V4: OK test")
+  @Test
+  void ontology_match_v4_page_OK() throws Exception {
     SimpleResponse response = getPostResponse("match/v3", matchPair);
     assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.code());
   }
