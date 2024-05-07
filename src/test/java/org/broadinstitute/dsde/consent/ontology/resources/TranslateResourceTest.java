64dbb62481e0baf32e463e3d8e6499ba3ab0b011
Gregory Rushton <rushtong@users.noreply.github.com>
2024-05-07 10:44:17 -0400

    [DCJ-324][risk=no] Add validation, tests, lower logging for Translate Resource (#959)

--- src/test/java/org/broadinstitute/dsde/consent/ontology/resources/TranslateResourceTest.java
+++ src/test/java/org/broadinstitute/dsde/consent/ontology/resources/TranslateResourceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 import com.google.gson.Gson;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
+import java.io.IOException;
 import java.util.HashMap;
 import org.broadinstitute.dsde.consent.ontology.datause.services.TextTranslationService;
 import org.broadinstitute.dsde.consent.ontology.enumerations.TranslateFor;
@@ -35,6 +36,34 @@ class TranslateResourceTest {
   }
 
   @Test
+  void testTranslateSummary() {
+    String restriction = new DataUseBuilder().setGeneralUse(true).toString();
+    try (Response response = resource.translateSummary(restriction)) {
+      assertEquals(200, response.getStatus());
+    }
+  }
+
+  @Test
+  void testTranslateSummaryNoRestriction() {
+    try (Response response = resource.translateSummary("")) {
+      assertEquals(400, response.getStatus());
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  void testTranslateSummaryException() {
+    when(service.translateDataUseSummary(any())).thenThrow(new RuntimeException());
+    String restriction = new DataUseBuilder().setGeneralUse(true).toString();
+    try (Response response = resource.translateSummary(restriction)) {
+      assertEquals(500, response.getStatus());
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
   void testDatasetTranslate() {
     DataUse datause = new DataUseBuilder().setGeneralUse(true).build();
     try (Response response = resource.translate(
@@ -42,6 +71,19 @@ class TranslateResourceTest {
         gson.toJson(datause))) {
       assertEquals(200, response.getStatus());
       verify(service, atLeastOnce()).translateDataset(any());
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  void testTranslateDatasetException() throws Exception {
+    when(service.translateDataset(any())).thenThrow(new IOException());
+    DataUse datause = new DataUseBuilder().setGeneralUse(true).build();
+    try (Response response = resource.translate(
+        TranslateFor.DATASET.name(),
+        gson.toJson(datause))) {
+      assertEquals(500, response.getStatus());
     } catch (Exception e) {
       fail(e.getMessage());
     }
@@ -61,6 +103,42 @@ class TranslateResourceTest {
   }
 
   @Test
+  void testTranslatePurposeException() throws Exception {
+    when(service.translatePurpose(any())).thenThrow(new IOException());
+    DataUse datause = new DataUseBuilder().setGeneralUse(true).build();
+    try (Response response = resource.translate(
+        TranslateFor.PURPOSE.name(),
+        gson.toJson(datause))) {
+      assertEquals(500, response.getStatus());
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  void testPurposeTranslateNoForParam() {
+    DataUse datause = new DataUseBuilder().setGeneralUse(true).build();
+    try (Response response = resource.translate(
+        null,
+        gson.toJson(datause))) {
+      assertEquals(400, response.getStatus());
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  void testPurposeTranslateNoForRestriction() {
+    try (Response response = resource.translate(
+        TranslateFor.PURPOSE.name(),
+        "")) {
+      assertEquals(400, response.getStatus());
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
   void testTranslateParagraphBadRequest() {
     try (Response response = resource.translateParagraph("{}")) {
       assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
@@ -70,7 +148,7 @@ class TranslateResourceTest {
   @Test
   void testTranslateParagraphServerError() {
     try (Response response = resource.translateParagraph("")) {
-      assertEquals(Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
+      assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
     }
   }
 
@@ -79,6 +157,19 @@ class TranslateResourceTest {
     when(service.translateParagraph(any())).thenReturn(new HashMap<>());
     try (Response response = resource.translateParagraph("{\"paragraph\":\"Not for Profit\"}")) {
       assertEquals(Status.OK.getStatusCode(), response.getStatus());
+    }
+  }
+
+  @Test
+  void testParagraphTranslateException() throws Exception {
+    when(service.translateParagraph(any())).thenThrow(new IOException());
+    DataUse datause = new DataUseBuilder().setGeneralUse(true).build();
+    try (Response response = resource.translate(
+        TranslateFor.PARAGRAPH.name(),
+        gson.toJson(datause))) {
+      assertEquals(500, response.getStatus());
+    } catch (Exception e) {
+      fail(e.getMessage());
     }
   }
 
